32afddaca9890b7170a42380d5436eba1beaaa68
sridharanitharan <username420sri@gmail.com>
2025-12-04 21:12:09 +0530

    Method clear() , in() , count()

--- src/main.c
+++ src/main.c
@@ -8,5 +8,26 @@ int main(){
     va.type = t_float;
     va.value._float = 32.5;
     list.append(&list,va);
+    list.clear(&list);
+    va.type = t_int;
+    va.value._int = 109;
+    list.append(&list,va);
+    va.type = t_int;
+    va.value._int = 109;
+    list.append(&list,va);
+    va.type = t_string;
+    va.value._string = "kobdfam ";
+    list.append(&list,va);
+    // va.type = t_string;
+    // va.value._string = "kobham";
+    // list.append(&list,va);
+    // va.type = t_string;
+    // va.value._string = "kobham";
+    // list.append(&list,va);
+    va.type = t_string;
+    va.value._string = "kobham";
+    
+    printf("count of %d\n",list.count(&list,va));
+    printf("if i in list %d",list.in(&list,va));
 
 }
--- src/pyclist.c
+++ src/pyclist.c
@@ -14,10 +14,55 @@ void _append(pyclist *list , data values){
             customErr(__func__,"Reallocation Failed",__LINE__,__FILE__);
         }
         list->capacity_size *=2;
+        list->data = temp;
     }
     list->data[list->init_size++] = values;
+    
 }
 
+int _clear(pyclist *list){
+
+    __init__(list);
+}
+
+
+int __search__(int *index ,data *list , data target , int count , int opt , int size){
+    if (list->type == target.type){
+        switch (target.type)
+        {
+        case t_int:
+            if (list->value._int == target.value._int){
+                count = count + 1;
+            }
+            break;
+        case t_float:
+            if (list->value._float == target.value._float){
+                count = count + 1;
+            }
+            break;
+        default:
+            if (list->value._string == target.value._string){
+                count = count + 1;
+            }
+        }
+    }
+
+    if ((opt || *index == size) && (count != 0 || *index == size)){
+        return count;
+    }
+    else{
+       count = count +  __search__(index,&list[(*index)++],target,count,opt,size);
+    }
+    return count;
+}
+int _in(pyclist *list,data values){
+    int *index;
+    return __search__(index,list->data,values,0,1,list->init_size);
+}
+int _count(pyclist *list, data values){
+    int *index;
+    return __search__(index,list->data,values,0,0,list->init_size-1); 
+}
 void __init__(pyclist *list){
     list->data = malloc(sizeof(data));
     if (list->data == NULL){
@@ -26,4 +71,7 @@ void __init__(pyclist *list){
     list->capacity_size = 1;
     list->init_size = 0;
     list->append = _append;
+    list->clear = _clear;
+    list->count = _count;
+    list->in = _in;
 }
