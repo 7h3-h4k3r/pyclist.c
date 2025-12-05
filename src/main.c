eea5ee56989d2ff5ca7d4ac8f1e6e03a1566a52d
sridharanitharan <username420sri@gmail.com>
2025-12-05 19:04:56 +0530

    len() isempty() index()

--- src/main.c
+++ src/main.c
@@ -9,25 +9,27 @@ int main(){
     va.value._float = 32.5;
     list.append(&list,va);
     list.clear(&list);
+    //=================================================================
     va.type = t_int;
     va.value._int = 109;
     list.append(&list,va);
     va.type = t_int;
     va.value._int = 109;
     list.append(&list,va);
-    va.type = t_string;
-    va.value._string = "kobdfam ";
+    data target = {.type=t_string,.value._string="kobdfam"};
+     va.type = t_string;
+    va.value._string = "kobham";
+    list.append(&list,va);  va.type = t_string;
+    va.value._string = "kobham";
     list.append(&list,va);
-    // va.type = t_string;
-    // va.value._string = "kobham";
-    // list.append(&list,va);
-    // va.type = t_string;
-    // va.value._string = "kobham";
-    // list.append(&list,va);
+    list.append(&list,target);
     va.type = t_string;
     va.value._string = "kobham";
-    
+    list.append(&list,va);
+    // printf("index of KObdfam %d",list.getindex(&list,n));
     printf("count of %d\n",list.count(&list,va));
-    printf("if i in list %d",list.in(&list,va));
-
+    printf("if i in list %d \n",list.in(&list,va));
+    printf("index of KObdfam %d\n",list.getindex(&list,target));
+    printf("Len of List %d \n",list.len(&list));
+    printf("is empty of List %d \n",list.isempty(&list));
 }
--- src/pyclist.c
+++ src/pyclist.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../header/include.h"
 
 void customErr(const char *func , char *msg , const int line , const char *file){
@@ -26,42 +27,69 @@ int _clear(pyclist *list){
 }
 
 
-int __search__(int *index ,data *list , data target , int count , int opt , int size){
-    if (list->type == target.type){
+int __search__(int *index ,data *list , data target ,int opt , int size){
+
+    if (*index >= size){
+        return 0;
+    }
+    int fount = -1;
+    if (list[*index].type == target.type){
         switch (target.type)
         {
         case t_int:
-            if (list->value._int == target.value._int){
-                count = count + 1;
+            if (list[*index].value._int == target.value._int){
+                fount = 1;
             }
             break;
         case t_float:
-            if (list->value._float == target.value._float){
-                count = count + 1;
+            if (list[*index].value._float == target.value._float){
+               fount = 1;
             }
             break;
         default:
-            if (list->value._string == target.value._string){
-                count = count + 1;
+            if (strcmp(list[*index].value._string,target.value._string)==0){
+                fount = 1;
             }
         }
     }
 
-    if ((opt || *index == size) && (count != 0 || *index == size)){
-        return count;
+    if ((opt == 1) && (fount == 1)){
+        return fount;
     }
-    else{
-       count = count +  __search__(index,&list[(*index)++],target,count,opt,size);
-    }
-    return count;
+    (*index)++;
+    int res = __search__(index,list,target,opt,size);
+    return fount + res;
 }
+
 int _in(pyclist *list,data values){
-    int *index;
-    return __search__(index,list->data,values,0,1,list->init_size);
+    int index = 0;
+    return __search__(&index,list->data,values,1,list->init_size);
 }
+
+int _len(pyclist *list ){
+    return list->init_size;
+}
+
+int _isempty(pyclist *list){
+    if (list->init_size == 0){
+        return 1;
+    }
+    return 0;
+}
+int  _getindex(pyclist *list,data value){
+    int index = 0;
+    int res = __search__(&index,list->data,value,1,list->init_size);
+    if (res == -1){
+        return -1;
+    }
+    return index;
+
+
+}   
 int _count(pyclist *list, data values){
-    int *index;
-    return __search__(index,list->data,values,0,0,list->init_size-1); 
+    int index = 0;
+    return __search__(&index,list->data,values,0,list->init_size-1);
+   
 }
 void __init__(pyclist *list){
     list->data = malloc(sizeof(data));
@@ -74,4 +102,7 @@ void __init__(pyclist *list){
     list->clear = _clear;
     list->count = _count;
     list->in = _in;
+    list->getindex = _getindex;
+    list->len = _len;
+    list->isempty = _isempty;
 }
